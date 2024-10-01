1ddae4b8169a0f6c071a91f3b00844d5f89e5039
Amirmohamad <amirmohamada017@gmail.com>
2024-10-01 14:14:07 +0330

    Add database models.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    Collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True)
+    membership = models.CharField(
+        max_length=1,
+        choices=(("B", "Bronze"), ("S", "Silver"), ("G", "Gold")),
+        default="B",
+    )
+
+
+class Order(models.Model):
+    placed_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    payment_status = models.CharField(
+        max_length=1,
+        choices=(("P", "Pending"), ("C", "Complete"), ("F", "Failed")),
+        default="P",
+    )
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True
+    )
